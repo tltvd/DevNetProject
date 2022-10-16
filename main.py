7782234db556e0b32e385c21aa38be69b22f4b73
tltvd <2448777B@gmail.com>
2022-10-16 22:43:34 +0600

    In this commit I move functions into ScannerIp class. It was necessary to make an unittest.

--- main.py
+++ main.py
@@ -4,6 +4,28 @@ import threading
 
 from queue import Queue
 
+
+class ScannerIp:
+    def __init__(self, ip):
+        self.ip = t_IP
+
+    def portscan(port):
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        try:
+            con = s.connect((t_IP, port))
+            with print_lock:
+                print(port, 'is open')
+            con.close()
+        except:
+            pass
+
+    def threader(self):
+        while True:
+            worker = q.get()
+            ScannerIp.portscan(worker)
+            q.task_done()
+
+
 socket.setdefaulttimeout(0.25)
 print_lock = threading.Lock()
 
@@ -11,27 +33,13 @@ target = input('Enter the host to be scanned: ')
 t_IP = socket.gethostbyname(target)
 print('Starting scan on host: ', t_IP)
 
-def portscan(port):
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    try:
-        con = s.connect((t_IP, port))
-        with print_lock:
-            print(port, 'is open')
-        con.close()
-    except:
-        pass
-
-def threader():
-    while True:
-        worker = q.get()
-        portscan(worker)
-        q.task_done()
+ip = ScannerIp(t_IP)
 
 q = Queue()
 startTime = time.time()
 
 for x in range(100):
-    t = threading.Thread(target=threader)
+    t = threading.Thread(target=ip.threader)
     t.daemon = True
     t.start()
 
