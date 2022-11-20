b7df63729f0cba892305088f04ab12510c6e3a63
tltvd <2448777B@gmail.com>
2022-11-20 23:00:57 +0600

    Add to program GUI and adding some functions.

--- TestMain.py
+++ TestMain.py
@@ -1,11 +1,35 @@
 import unittest
 import socket
+import urllib.request
 from main import *
+
+
+def try_urlopen(link):
+    try:
+        urllib.request.urlopen(link)
+        return True
+    except Exception as ex:
+        print(ex)
+    return False
+
+
+def network_connection_check(link):
+    if not link.startswith('http'):
+        for prefix in ['http://', 'https://']:
+            if try_urlopen(prefix + link):
+                return True
+        return False
+    else:
+        return urllib.request.urlopen(link)
 
 
 class TestMain(unittest.TestCase):
     def test_IfIpIsTrue(self):
         self.assertTrue(socket.inet_aton(t_IP))
+
+    def test_connect(self):
+        self.assertTrue(network_connection_check(target))
+
 
 if __name__ == "__main__":
     unittest.main()
