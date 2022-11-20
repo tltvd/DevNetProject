85438f0a90651102a9d175b740231a7d7d2432cf
tltvd <2448777B@gmail.com>
2022-11-20 23:00:57 +0600

    Resolve some problems

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import threading
 
 from queue import Queue
 
+import requests
+
 
 class ScannerIp:
     def __init__(self, ip):
@@ -26,25 +28,60 @@ class ScannerIp:
             q.task_done()
 
 
-socket.setdefaulttimeout(0.25)
-print_lock = threading.Lock()
+    print('''\033[1;97m██████╗░███████╗░█████╗░██████╗░
+██╔══██╗██╔════╝██╔══██╗██╔══██╗
+██████╔╝█████╗░░███████║██║░░██║
+██╔══██╗██╔══╝░░██╔══██║██║░░██║
+██║░░██║███████╗██║░░██║██████╔╝
+╚═╝░░╚═╝╚══════╝╚═╝░░╚═╝╚═════╝\033[0m\n''')
 
-target = input('Enter the host to be scanned: ')
-t_IP = socket.gethostbyname(target)
-print('Starting scan on host: ', t_IP)
 
-ip = ScannerIp(t_IP)
+print("""Welcome to our DevNet project work! Our program is focused on network analysis and troubleshooting.
+Functions of this program:
+===============================================================================================================
+1) Network scanner
+This program scans the victim to locate open ports available on a particular host.
+===============================================================================================================
+2) Checking the availability of HTTP methods
+Given Python crypt with which we can connect to the target web server and enumerate the available HTTP methods.
+===============================================================================================================
+""")
+choice=int(input('Please, enter the number of the feature you want to use: '))
+if choice==1:
+    socket.setdefaulttimeout(0.25)
+    print_lock = threading.Lock()
+    target = input('Enter the host to be scanned: ')
+    t_IP = socket.gethostbyname(target)
+    print('Starting scan on host: ', t_IP)
 
-q = Queue()
-startTime = time.time()
+    ip = ScannerIp(t_IP)
 
-for x in range(100):
-    t = threading.Thread(target=ip.threader)
-    t.daemon = True
-    t.start()
+    q = Queue()
+    startTime = time.time()
 
-for worker in range(1, 500):
-    q.put(worker)
+    for x in range(100):
+        t = threading.Thread(target=ip.threader)
+        t.daemon = True
+        t.start()
 
-q.join()
-print('Time taken:', time.time() - startTime)
+    for worker in range(1, 500):
+        q.put(worker)
+
+    q.join()
+if choice==2:
+    target = input('Enter the host to be scanned: ')
+    method_list = ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'TRACE', 'TEST']
+    for method in method_list:
+        req = requests.request(method, target)
+        print(method, req.status_code, req.reason)
+    if method == 'TRACE' and 'TRACE / HTTP/1.1' in req.text:
+        print('Cross Site Tracing(XST) is possible')
+
+
+
+
+
+
+
+
+
